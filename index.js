f2b780cbba87b823d7ecfe109de71380783063fa
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-07 00:02:28 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() + 1);
+
+let num3 = 9;
+let num4 = 5
+let mod = num3 % num4;
+
+let x;
+let max = Math.max(20);
